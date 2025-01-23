f0d870f50f4496a6cde8bd18130aa75fbe60b634
Rae2480 <raeedareu@gmail.com>
2025-01-23 14:49:33 +0800

    Add customizations & credit AI
    Both display and responses are now different from DUKE.
    Responses are not randomized from a range of responses.
    Observations wrt AI use recorded

--- src/main/java/Viktor.java
+++ src/main/java/Viktor.java
@@ -1,37 +1,49 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Viktor {
     public static void main(String[] args) {
         String name = "Viktor";
-        String logo = "      .__ __      __                \n"
-                + "___  _|__|  | ___/  |_  ___________ \n"
-                + "\\  \\/ /  |  |/ /\\   __\\/  _ \\_  __ \\\n"
-                + " \\   /|  |    <  |  | (  <_> )  | \\/\n"
-                + "  \\_/ |__|__|_ \\ |__|  \\____/|__|   \n"
-                + "              \\/                    ";
+        String logo = "\n" 
+                + "\t" + "      .__ __      __                \n"
+                + "\t" + "___  _|__|  | ___/  |_  ___________ \n"
+                + "\t" + "\\  \\/ /  |  |/ /\\   __\\/  _ \\_  __ \\\n"
+                + "\t" + " \\   /|  |    <  |  | (  <_> )  | \\/\n"
+                + "\t" + "  \\_/ |__|__|_ \\ |__|  \\____/|__|   \n"
+                + "\t" + "              \\/                    \n";
         System.out.println(
-            "----------------------------------------------------------\n" + 
+            "\n⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅\n\n" + 
             "Hello! I'm " + name + "\n" + logo + "\n" +
             "What can I do for you?\n" +
-            "----------------------------------------------------------\n");
+            "\n\n⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅\n");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("\n");
-        String userInput = scanner.nextLine();
+        Random random = new Random();
+        boolean running = true;
 
-        while (!userInput.equals("bye")) {
-            System.out.println(
-                "----------------------------------------------------------\n" +
-                "\t" + userInput + "\n" +
-                "----------------------------------------------------------\n");
-            userInput = scanner.nextLine();
-        } 
-        System.out.println(
-            "----------------------------------------------------------\n" +
-            "Bye. Hope to see you again soon!" + "\n" +
-            "----------------------------------------------------------\n");
+        while (running) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("\n⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅\n\n" +
+                "Goodbye. Remember, progress never rests." +
+                "\n\n⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅\n");
+                running = false;
+            } else {
+                String[] responses = {
+                    "The pursuit of knowledge is a noble endeavor. So is",
+                    "Progress requires sacrifice, but it is always worth the cost for",
+                    "Even the greatest minds require assistance sometimes. You seek to know about",
+                    "Ah, progress—the double-edged sword of innovation, alongside:",
+                    "Every leap forward brings challenges, but such is the price of",
+                    "Interesting... Perhaps you could explain further about this"
+                };
+                String response = responses[random.nextInt(responses.length)];
+                System.out.println("\n⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅\n\n" +
+                response + " " + userInput + "." + "\n\n⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅⋅.˳˳.⋅ॱ˙˙ॱ⋅.˳˳.⋅ॱ˙˙ॱᐧ.˳˳.⋅\n");
+            }
+        }
 
         scanner.close();
-
     }
 }
